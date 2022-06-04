24750c49280dfb2510c343d1036a0cb9c250b46f
LuciPl <lucianoplouvier@id.uff.br>
2022-06-04 13:39:28 -0300

    gitignore

--- TCC/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <random>
-#include <time.h>
-
-namespace Utils {
-	int getRandomInt(int minPossibleValue, int maxPossibleValue);
-}
--- TCC/structuralMethods.cpp
+++
@@ -1,5 +0,0 @@
-#include "structuralMethods.h"
-
-void StructuralMethods::sequentialInsertion(int s, int v, int candidatesList, InsertionCriterion criteria) {
-
-}
